e1108d86a05b27e2d88fdc3fc8021213ad8c62de
Blaise Notter <59936853+bigbulse@users.noreply.github.com>
2021-09-17 14:31:23 -0400

    Update dns-resolver.ps1

--- dns-resolver.ps1
+++ dns-resolver.ps1
@@ -1,5 +1,5 @@
-$network=$arg[0]
-$dnsserver=$arg[1]
+$network=$args[0]
+$dnsserver=$args[1]
 for (($i = 1); ($i -lt 256); ($i++)){
          $address=$network+"."+$i
          Resolve-DnsName -DnsOnly $address -Server $dnsserver -ErrorAction Ignore
