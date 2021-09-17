ce1effa69e687341db79765079e416c6e3ff5145
Blaise Notter <59936853+bigbulse@users.noreply.github.com>
2021-09-17 14:25:51 -0400

    Create dns-resolver.ps1

---
+++ dns-resolver.ps1
@@ -0,0 +1,5 @@
+$network=$arg[0]
+$dnsserver=$arg[1]
+for (($i = 1); ($i -lt 256); ($i++)){
+         $address=$network+"."+$i
+         Resolve-DnsName -DnsOnly $address -Server $dnsserver -ErrorAction Ignore
